85d3dc42cc45517c29845a819c941fd3c675c741
Constantine <kostyaxxx8@gmail.com>
2019-04-21 16:51:48 +0300

    Create base ui logic

--- app/src/main/java/ru/semper_viventem/exchangerates/ui/MainPm.kt
+++ app/src/main/java/ru/semper_viventem/exchangerates/ui/MainPm.kt
@@ -1,30 +1,51 @@
 package ru.semper_viventem.exchangerates.ui
 
-import io.reactivex.functions.Consumer
+import io.reactivex.Observable
+import io.reactivex.rxkotlin.withLatestFrom
 import me.dmdev.rxpm.PresentationModel
 import ru.semper_viventem.exchangerates.domain.CurrencyEntity
 import ru.semper_viventem.exchangerates.domain.GetExchangeRatesInteractor
 import timber.log.Timber
+import java.util.concurrent.TimeUnit
 
 class MainPm(
     private val getExchangeRatesInteractor: GetExchangeRatesInteractor
 ) : PresentationModel() {
 
+    companion object {
+        private const val UPDATE_INTERVAL_MILLISECONDS = 1000L
+    }
+
     val rate = State<List<CurrencyEntity>>()
     val currencySelected = Action<CurrencyEntity>()
 
-    override fun onCreate() {
-        super.onCreate()
-
-        val baseCurrency = CurrencyEntity(
+    private val baseCurrency = State(
+        CurrencyEntity(
             name = "EUR",
             fullName = "Euro",
             value = 1.0,
             imageRes = null
         )
+    )
 
-        getExchangeRatesInteractor.execute(baseCurrency)
-            .subscribe(rate.consumer, Consumer { error -> Timber.e(error) })
+    val timer = Observable.interval(UPDATE_INTERVAL_MILLISECONDS, TimeUnit.MILLISECONDS)
+
+    override fun onCreate() {
+        super.onCreate()
+
+        currencySelected.observable
+            .subscribe(baseCurrency.consumer)
+            .untilDestroy()
+
+        timer.withLatestFrom(baseCurrency.observable) { _, base -> base }
+            .flatMapSingle { baseCurrency ->
+                getExchangeRatesInteractor.execute(baseCurrency)
+                    .map { listOf(baseCurrency) + it }
+            }
+            .doOnNext(rate.consumer)
+            .doOnError { error -> Timber.e(error) }
+            .retry()
+            .subscribe()
             .untilDestroy()
     }
 }
