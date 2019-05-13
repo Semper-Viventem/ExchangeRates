dea443326f3ee749bbece64f36bc1c5d80d9ee54
Constantine <kostyaxxx8@gmail.com>
2019-05-13 21:42:43 +0300

    Fix MainActivity test

--- app/src/androidTest/java/ui/MainActivityTest.kt
+++ app/src/androidTest/java/ui/MainActivityTest.kt
@@ -12,8 +12,9 @@ import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.koin.core.context.loadKoinModules
+import org.koin.dsl.bind
 import org.koin.dsl.module
-import org.koin.test.AutoCloseKoinTest
+import org.koin.test.KoinTest
 import ru.semper_viventem.exchangerates.R
 import ru.semper_viventem.exchangerates.data.network.Api
 import ru.semper_viventem.exchangerates.data.network.response.ExchangeRatesResponse
@@ -22,10 +23,10 @@ import java.net.UnknownHostException
 
 @RunWith(AndroidJUnit4::class)
 @LargeTest
-class MainActivityTest : AutoCloseKoinTest() {
+class MainActivityTest : KoinTest {
 
     @get:Rule
-    val activityRule = ActivityTestRule(MainActivity::class.java)
+    val activityRule = ActivityTestRule(MainActivity::class.java, false, false)
 
     class FakeApi : Api {
         override fun latest(base: String?): Single<ExchangeRatesResponse> {
@@ -36,22 +37,25 @@ class MainActivityTest : AutoCloseKoinTest() {
     }
 
     @Before
-    fun before() {
-        loadKoinModules(
-            module { single(override = true) { FakeApi() }
+    fun setUp() {
+        loadKoinModules(module {
+            single(override = true) { FakeApi() } bind Api::class
         })
     }
 
     @Test
-    fun testErrorMessageIfNoInternet() {
-
-        onView(withId(com.google.android.material.R.id.snackbar_text))
-            .check(matches(withText(R.string.error_no_internet_connection)))
+    fun testDataInLoading() {
+        activityRule.launchActivity(null)
+        onView(withId(R.id.progress))
+            .check(matches(isDisplayed()))
     }
 
     @Test
-    fun testDataInLoading() {
-        onView(withId(R.id.progress))
+    fun testErrorMessageIfNoInternet() {
+        activityRule.launchActivity(null)
+        Thread.sleep(1000L)
+        onView(withId(com.google.android.material.R.id.snackbar_text))
             .check(matches(isDisplayed()))
+            .check(matches(withText(R.string.error_no_internet_connection)))
     }
 }
